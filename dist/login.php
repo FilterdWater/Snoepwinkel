8479c3a13f2fff81446696c5625c2920de864da8
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2023-06-16 14:54:21 +0200

    End of day checkin

--- dist/login.php
+++ dist/login.php
@@ -12,9 +12,9 @@
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
     <script defer src="mobile-menu.js"></script>
 </head>
-
 <body>
-    <div class="bg-white">
+
+<div class="bg-white">
         <header class="absolute inset-x-0 top-0 z-50">
             <nav class="fixed top-0 w-full flex items-center justify-between p-3 lg:px-8 bg-white outline outline-2 outline-cyan-700" aria-label="Global">
                 <div class="flex lg:flex-1">
@@ -93,55 +93,51 @@
             </div>
         </header>
 
-
-        <section style="background-image: url(images/6328794.webp);">
-            <div class="px-4 py-40">
-                <div class="mx-auto max-w-xl text-left bg-white/90 px-8 py-16 rounded">
-                    <h1 class="text-3xl sm:text-5xl font-extrabold"> Welcome op <div class="sm:block font-extrabold text-rose-500">Candyshop CandyPop</div>
-                    </h1>
-                    <p class="mt-4 sm:text-xl/relaxed"> De meest heerlijke en snelst bezorgde snoepjes. </p>
-                </div>
-            </div>
-        </section>
+<div class="mt-28 mx-12 sm:mx-auto sm:w-full sm:max-w-sm">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mx-auto h-20 w-auto mt-10" alt="user icon">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
 
 
-        <section class="bg-white text-black">
-            <div class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
-                <div class="mx-auto max-w-lg text-center">
-                    <h2 class="text-3xl font-bold sm:text-4xl">Fijn dat je er bent</h2>
-                    <p class="mt-4 text-gray-900"> Wij zijn de snelst groeiende online zoet verkoper op de markt. Met een gigantisch assortiment van snoepjes kan je alles vinden wat je maar kan bedenken. Onze razend snelle services zal je verbaast stellen. </p>
-                </div>
-                <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-                    <a class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-rose-500/10 hover:shadow-rose-500/10" href="/">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 ">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
-                        </svg>
-                        <h2 class="mt-4 text-xl font-bold">Snoep en Zoetigheid</h2>
-                        <p class="mt-1 text-sm text-gray-900"> Wij hebben het beste assortiment van snoepjes. Alles wat je van ons koopt is top kwaliteit. Dus koop wat snoepjes. </p>
-                        <div class="text-xs mt-5">Click for more</div>
-                    </a>
-                    <a class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-rose-500/10 hover:shadow-rose-500/10" href="/">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w10 h-10">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                        </svg>
-                        <h2 class="mt-4 text-xl font-bold">Onze contact gegevens</h2>
-                        <p class="mt-1 text-sm text-gray-900"> Will jij met ons praten? Dat kan! hier vind je onze contact informatie zodat je ons kan bereiken. </p>
-                        <div class="text-xs mt-5">Click for more</div>
-                    </a>
-                    <a class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-rose-500/10 hover:shadow-rose-500/10" href="/">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                        </svg>
-                        <h2 class="mt-4 text-xl font-bold">Wie zijn wij</h2>
-                        <p class="mt-1 text-sm text-gray-900"> Weet je nog niet wie we zijn? Op deze pagina kan je meer over ons leren. Mischien leer je iets nieuws.</p>
-                        <div class="text-xs mt-5">Click for more</div>
-                    </a>
-                </div>
-            </div>
-        </section>
+    
+    <form class="space-y-6 pb-14" action="/" method="POST">
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Gebruikersnaam</label>
+        </div>
+        <div class="mt-2">
+          <input type="text" name="username" id="username" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
 
 
-        <footer class="bg-rose-400">
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div class="mt-2">
+          <input type="text" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+      
+      <div>
+        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+        <div class="mt-2">
+          <input type="text" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+
+      
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Reserveer</button>
+      </div>
+    </form>
+</div>
+</div>
+
+<footer class="bg-rose-400">
             <div class="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
                 <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
                     <div class="bg-rose-500 shadow-inner rounded p-4">
@@ -208,6 +204,7 @@
                 </ul>
             </div>
         </footer>
-</body>
 
+
+</body>
 </html>
