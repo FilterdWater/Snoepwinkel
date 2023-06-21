cb23f8fdc4d8e510309344b7883f09bd9608741d
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2023-06-21 10:45:17 +0200

    php footer/header

---
+++ dist/header.php
@@ -0,0 +1,77 @@
+        <header class="absolute inset-x-0 top-0 z-50">
+        <nav class="fixed top-0 w-full flex items-center justify-between p-3 lg:px-8 bg-white outline outline-2 outline-cyan-700" aria-label="Global">
+            <div class="flex lg:flex-1">
+                <a href="index.php" class="-m-1.5 p-1.5">
+                    <span class="sr-only">Logo</span>
+                    <div class="block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner"><img src="images/android-chrome-512x512.png" alt="Logo candypop" style="max-width: 2em;"></div>
+                </a>
+            </div>
+            <div class="flex lg:hidden">
+                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onclick="show()">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+            </div>
+            <div class="hidden lg:flex lg:gap-x-12">
+                <a href="index.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-gray-300 hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner">Home</a>
+                <a href="snoep.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-gray-300 hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner">Snoep</a>
+                <a href="contact.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-gray-300 hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner">Contact</a>
+                <a href="about.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-gray-300 hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner">Over ons</a>
+            </div>
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+                <a href="shopping-cart.php" class="block rounded-lg px-2 py-2 mx-1 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>
+                </a>
+                <a href="login.php" class="block rounded-lg px-2 py-2 mx-1 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </a>
+            </div>
+        </nav>
+        <!-- Mobile menu -->
+        <div class="lg:hidden" role="dialog" aria-modal="true">
+            <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 navigation">
+                <div class="flex items-center justify-between">
+                    <a href="index.php" class="-m-1.5 p-1.5">
+                        <span class="sr-only">Logo</span>
+                        <div class="block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all shadow-inner"><img src="images/android-chrome-512x512.png" alt="Logo candypop" style="max-width: 2em;"></div>
+                    </a>
+                    <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" onclick="show()">
+                        <span class="sr-only">Close menu</span>
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="mt-6 flow-root">
+                    <div class="-my-6 divide-y divide-gray-500/10">
+                        <div class="space-y-2 py-6">
+                            <a href="index.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all">Home</a>
+                            <a href="snoep.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all">Snoep</a>
+                            <a href="contact.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all">Contact</a>
+                            <a href="about.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all">Over ons</a>
+                        </div>
+                        <div class="py-6">
+                            <a href="login.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div class="py-6">
+                            <a href="shopping-cart.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold border-b-2 border-transparent hover:border-current cursor-pointer select-none text-gray-900 hover:bg-gray-50 transition-all">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ dist/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | CandyPop</title>
+    <link rel="stylesheet" href="output.css">
+    <link rel="stylesheet" href="custom.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <script defer src="mobile-menu.js"></script>
+</head>
+
+<body>
+    <div class="bg-white">
+
+    <?php
+    
+    require_once 'header.php'
+
+    ?>
+
+        <section style="background-image: url(images/6328794.webp);">
+            <div class="px-4 py-40">
+                <div class="mx-auto max-w-xl text-left bg-white/90 px-8 py-16 rounded">
+                    <h1 class="text-3xl sm:text-5xl font-extrabold"> Welcome op <div class="sm:block font-extrabold text-rose-500">Candyshop CandyPop</div>
+                    </h1>
+                    <p class="mt-4 sm:text-xl/relaxed"> De meest heerlijke en snelst bezorgde snoepjes. </p>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="bg-white text-black">
+            <div class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+                <div class="mx-auto max-w-lg text-center">
+                    <h2 class="text-3xl font-bold sm:text-4xl">Fijn dat je er bent</h2>
+                    <p class="mt-4 text-gray-900"> Wij zijn de snelst groeiende online zoet verkoper op de markt. Met een gigantisch assortiment van snoepjes kan je alles vinden wat je maar kan bedenken. Onze razend snelle services zal je verbaast stellen. </p>
+                </div>
+                <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                    <a class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-rose-500/10 hover:shadow-rose-500/10" href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 ">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
+                        </svg>
+                        <h2 class="mt-4 text-xl font-bold">Snoep en Zoetigheid</h2>
+                        <p class="mt-1 text-sm text-gray-900"> Wij hebben het beste assortiment van snoepjes. Alles wat je van ons koopt is top kwaliteit. Dus koop wat snoepjes. </p>
+                        <div class="text-xs mt-5">Click for more</div>
+                    </a>
+                    <a class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-rose-500/10 hover:shadow-rose-500/10" href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w10 h-10">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                        </svg>
+                        <h2 class="mt-4 text-xl font-bold">Onze contact gegevens</h2>
+                        <p class="mt-1 text-sm text-gray-900"> Will jij met ons praten? Dat kan! hier vind je onze contact informatie zodat je ons kan bereiken. </p>
+                        <div class="text-xs mt-5">Click for more</div>
+                    </a>
+                    <a class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-rose-500/10 hover:shadow-rose-500/10" href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                        </svg>
+                        <h2 class="mt-4 text-xl font-bold">Wie zijn wij</h2>
+                        <p class="mt-1 text-sm text-gray-900"> Weet je nog niet wie we zijn? Op deze pagina kan je meer over ons leren. Mischien leer je iets nieuws.</p>
+                        <div class="text-xs mt-5">Click for more</div>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+
+    <?php
+    
+    require_once 'footer.php'
+
+    ?>
+
+</body>
+
+</html>
