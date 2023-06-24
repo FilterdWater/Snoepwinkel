878206ce2920d1f8356f285aa868f09c5514013f
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2023-06-24 16:09:01 +0200

    db.dist.php veranderd

--- dist/db.dist.php
+++ dist/db.dist.php
@@ -2,57 +2,77 @@
 require_once('db.inc.php');
 $con = getDBConnection();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['name'];
-  $price = $_POST['price'];
-  $description = $_POST['description'];
-  $detailed_description = $_POST['detailed_description'];
-  $nutritional_value = $_POST['nutritional_value'];
-  $filter_category = $_POST['filter_category'];
+function validateFile($file) //Checkt of de foto die gestuurd well mag gestuurd worden
+{
+    $allowedExtensions = array('jpg', 'jpeg', 'webp');
+    $maxFileSize = 1 * 1024 * 1024; // 1 MB
 
-  // Foto en foto filepath Uploaden naar db
-  $targetDir = "images/";
-  $fileName = $_FILES['picture']['name'];
-  $targetFilePath = $targetDir . $fileName;
+    $fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+    $fileSize = $file['size'];
 
-  // Check if uploaded file has an allowed file extension
-  $allowedExtensions = array('jpg', 'jpeg', 'WEBP');
-  $fileExtension = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
-  if (!in_array($fileExtension, $allowedExtensions)) {
-    echo "Error: Only JPG, JPEG, and WEBP files are allowed.";
-    exit();
-  }
+    if (!in_array($fileExtension, $allowedExtensions)) {
+        return "Error: Only JPG, JPEG, and WEBP files are allowed.";
+    }
 
-  // Check file size
-  $maxFileSize = 1 * 1024 * 1024; // 1 MB     1024x1024 = 1mb
-  $fileSize = $_FILES['picture']['size'];
-  if ($fileSize > $maxFileSize) {
-    echo "Error: The file size exceeds the maximum limit.";
-    exit();
-  }
+    if ($fileSize > $maxFileSize) {
+        return "Error: The file size exceeds the maximum limit.";
+    }
 
-  move_uploaded_file($_FILES['picture']['tmp_name'], $targetFilePath);
-
-  $sql = "INSERT INTO snoepjes (name, picture, price, description, detailed_description, nutritional_value, filter_category) VALUES (:name, :picture, :price, :description, :detailed_description, :nutritional_value, :filter_category)";
-  $stmt = $con->prepare($sql);
-
-  $stmt->bindParam(':name', $name);
-  $stmt->bindParam(':picture', $targetFilePath);
-  $stmt->bindParam(':price', $price);
-  $stmt->bindParam(':description', $description);
-  $stmt->bindParam(':detailed_description', $detailed_description);
-  $stmt->bindParam(':nutritional_value', $nutritional_value);
-  $stmt->bindParam(':filter_category', $filter_category);
-
-  try {
-    $stmt->execute();
-    echo "Data inserted successfully";
-    header('Location: success-product-toevoegen.php');
-    exit();
-  } catch(PDOException $e) {
-    echo "Error inserting data: " . $e->getMessage();
-  }
+    return null; // File validation successful
 }
 
+function processFormSubmission($con)
+{
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $detailedDescription = $_POST['detailed_description'];
+        $nutritionalValue = $_POST['nutritional_value'];
+        $filterCategory = $_POST['filter_category'];
+
+        if (!isset($_FILES['picture'])) {
+            echo "Error: No file uploaded.";
+            return;
+        }
+
+        $file = $_FILES['picture'];
+
+        $fileValidationResult = validateFile($file);
+        if ($fileValidationResult !== null) {
+            echo $fileValidationResult;
+            return;
+        }
+
+        $targetDir = "images/";
+        $targetFileName = uniqid() . '_' . $file['name'];
+        $targetFilePath = $targetDir . $targetFileName;
+
+        move_uploaded_file($file['tmp_name'], $targetFilePath);
+
+        $sql = "INSERT INTO snoepjes (name, picture, price, description, detailed_description, nutritional_value, filter_category) VALUES (:name, :picture, :price, :description, :detailed_description, :nutritional_value, :filter_category)";
+        $stmt = $con->prepare($sql);
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':picture', $targetFilePath);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':detailed_description', $detailedDescription);
+        $stmt->bindParam(':nutritional_value', $nutritionalValue);
+        $stmt->bindParam(':filter_category', $filterCategory);
+
+        try {
+            $stmt->execute();
+            echo "Data inserted successfully";
+            header('Location: success-product-toevoegen.php');
+            exit();
+        } catch(PDOException $e) {
+            echo "Error inserting data: " . $e->getMessage();
+            error_log("Error inserting data: " . $e->getMessage());
+        }
+    }
+}
+
+processFormSubmission($con);
 $con = null;
 ?>
